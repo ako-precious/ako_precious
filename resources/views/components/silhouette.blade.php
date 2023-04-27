dc6f6771ee736e56db7c05419c5a2a51671344e4
AKO PRECIOUS <akoprecious58@gmail.com>
2023-04-27 23:23:30 +0100

    more designs

--- resources/views/components/silhouette.blade.php
+++ resources/views/components/silhouette.blade.php
@@ -1,3 +1,17 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+@PushOnce('styles')
+@endPushOnce('styles')
+
+<div class="container bg-blue-900 flex justify-center items-center flex-wrap">
+    <div class="box relative m-10 " style="height:325px; width:400px">
+        <img src="{{ asset('images/akoprecious.png') }}"
+            class=" absolute top-0 left-0 w-full h-full object-cover mix-blend-screen" alt="">
+        <img src="{{ asset('images/nigeria.png') }}"
+            class=" bg-transparent absolute top-0 left-0 w-full h-full object-cover mix-blend-screen overflow-hidden"
+            alt="nigeria map">
+    </div>
+
 </div>
+
+
+@PushOnce('scripts')
+@endPushOnce
